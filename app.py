0933643a97088fd046726eca36cff94f61ccd791
nicholas-martino <nicholas.martino@hotmail.com>
2021-12-01 23:13:45 -0800

    Gunicorn route fix

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import dash
 from layout.layout import layout
+import dash_bootstrap_components as dbc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = layout
 
 server = app.server
