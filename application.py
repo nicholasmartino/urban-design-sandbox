ee9aba58b9e3e13ee4da23b246d3b198188880b8
Nicholas Martino <nicholasmartino@host212-99.vpn.ubc.ca>
2021-12-15 10:43:40 -0800

    Deploy AWS

--- application.py
+++ application.py
@@ -457,6 +457,4 @@ def download_layers(deck_div, n_clicks):
 
 
 if __name__ == '__main__':
-	app.run_server(debug=False, host='0.0.0.0', port=80)
-
-"https://www.phillipsj.net/posts/deploying-dash-to-azure-app-service/"
+	app.run_server(debug=True, port=8080)
